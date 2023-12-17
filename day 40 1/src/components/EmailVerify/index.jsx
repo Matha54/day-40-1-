eebf9dd7255673bf3eceb6c3d47b6ba178942c85
Mathanraj <131568872+Matha54@users.noreply.github.com>
2023-12-17 04:27:22 -0800

    Add files via upload

---
+++ day 40 1/src/components/EmailVerify/index.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import success from '../../images/success.png';
+import styles from './styles.module.css';
+
+const EmailVerify = () => {
+  const [validUrl, setValidUrl] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const { id, token } = useParams();
+
+  useEffect(() => {
+    const verifyEmailUrl = async () => {
+      try {
+        const url = `http://localhost:8000/api/users/${id}/verify/${token}`;
+        const { data } = await axios.get(url);
+        console.log(data);
+        setValidUrl(true);
+      } catch (error) {
+        console.log(error);
+        setValidUrl(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyEmailUrl();
+  }, [id, token]);
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <p>Loading...</p>
+      ) : validUrl ? (
+        <>
+          <img src={success} alt="success_img" className={styles.success_img} />
+          <h1>Email verified successfully</h1>
+          <Link to="/login">
+            <button className={styles.green_btn}>Login</button>
+          </Link>
+        </>
+      ) : (
+        <h1>404 Not Found</h1>
+      )}
+    </div>
+  );
+};
+
+export default EmailVerify;
---
+++ day 40 1/src/components/Main/index.jsx
@@ -0,0 +1,75 @@
+import styles from "./styles.module.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+	const [url, setUrl] = useState('');
+	const [shortId, setShortId] = useState('');
+
+	const handleShortenURL = async () => {
+		try {
+			const response = await axios.post('https://urlshortner-6eme.onrender.com/api/url', { url });
+			setShortId(response.data.id);
+		} catch (error) {
+			console.error('Error shortening URL:', error);
+		}
+	};
+
+
+	return (
+		<div>
+			<div className={styles.main_container}>
+				<nav className={styles.navbar}>
+					<h1>Welcome Guvi</h1>
+					<button className={styles.white_btn} onClick={handleLogout}>
+						Logout
+					</button>
+				</nav>
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '20vh' }}>
+				<input type="text" value={url} onChange={(e) => setUrl(e.target.value)} style={{
+					marginBottom: '10px',
+					outline: 'none',
+					border: 'none',
+					width: '370px',
+					padding: '15px',
+					borderRadius: '10px',
+					backgroundColor: '#edf5f3',
+					margin: '5px 0',
+					fontSize: '14px',
+				}} />
+				<br />
+				<button onClick={handleShortenURL} style={{
+					marginBottom: '10px',
+					border: 'none',
+					outline: 'none',
+					padding: '12px 0',
+					backgroundColor: '#3bb19b',
+					color: 'white',
+					borderRadius: '20px',
+					width: '180px',
+					fontWeight: 'bold',
+					fontSize: '14px',
+					cursor: 'pointer',
+					margin: '10px auto',
+				}}>Shorten URL</button>
+				<br />
+				{shortId && <p
+				style={{
+					marginBottom: '10px',
+					marginRight:'auto',
+					marginLeft:'auto',
+					fontFamily: 'Helvetica',
+					fontSize: '25px',
+				}}
+				>https://urlshortner-6eme.onrender.com/{shortId}</p>}
+			</div>
+		</div>
+	);
+};
+
+export default Main;
